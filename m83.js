bb2c39a5ff9355a1dcd245f8ea269648abaf3bae
M83 <75308710+M83XD@users.noreply.github.com>
2023-03-20 15:14:57 +0300

    Create m83.js

---
+++ m83.js
@@ -0,0 +1,10 @@
+function diziAc() {
+    const dizi = ["M83","Shara","discord.gg/shara","JavaScript"]
+dizi.push
+    console.log("Değerler : " + dizi)
+}
+
+function sil() {
+    dizi = []
+    console.log("Değerler : " + dizi)
+}
